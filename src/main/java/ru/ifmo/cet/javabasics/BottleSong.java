7a279d9594b7f6110233a2a1ea6abb8c68609aa3
Александр <kotekjedi>
2018-10-11 19:43:14 +0300

    task-1 Panfilov P3221

--- src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
+++ src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
@@ -1,5 +1,8 @@
 package ru.ifmo.cet.javabasics;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Нужно реализовать констурктор и метод, возвращающий слова песни про бутылки на стене.
  * <p>
@@ -32,12 +35,70 @@ package ru.ifmo.cet.javabasics;
  */
 public class BottleSong {
 
+    private int bottles;
+
+
     public BottleSong(int bottleTakenAtOnce) {
         //TODO
+         if (bottleTakenAtOnce<100 && bottleTakenAtOnce>0) {bottles=bottleTakenAtOnce;}
     }
 
     public String getBottleSongLyrics() {
         //TODO
-        throw new UnsupportedOperationException();
+        // * 3 bottles of beer on the wall, 3 bottles of beer
+        // * Take one down, pass it around, 2 bottles of beer
+
+        if (bottles > 99 || bottles < 1) {
+            throw new IllegalArgumentException();
+        }
+        String [] ones = {"","one ","two ","three ","four ","five ","six ","seven ","eight ","nine ","ten "};
+        String [] ones1 = {"","one","two","three","four","fif","six","seven","eight","nine","ten"};
+        String [] tens = {"","teen ","twenty ","thirty ","forty ","fifty ","sixty ","seventy ","eighty ","ninety ","hundred"};
+        String spelling = null;
+        String wall = " bottles of beer on the wall, ";
+        String part = " bottles of beer on the wall.\n";
+        if(bottles > 19){
+            spelling =(tens[(bottles)/10]+ ones[bottles%10]);
+        }
+        else if (bottles>13) {spelling =ones1[bottles%10]+"teen ";}
+        else if (bottles==11) {spelling ="eleven ";}
+        else if (bottles==12) {spelling ="twelve ";}
+        else if (bottles==13) {spelling ="thirteen ";}
+        else{
+            spelling =(ones[bottles]);
+        }
+
+        String end = " bottles of beer.\nTake " + spelling + "down and pass around, ";
+        int current = 99;
+        StringBuilder b = new StringBuilder();
+       // b.append(current).append(wall).append(current).append(end).append(current-bottles).append(part);
+        while (current >bottles) {
+            b.append(current).append(wall).append(current).append(end).append(current-bottles);
+                    if (current-bottles!=1) {b.append(part);}
+                    else {b.append(" bottle of beer on the wall.\n");}
+            current-=bottles;
+        }
+        if(current > 19){
+            spelling =(tens[(current)/10]+ ones[current%10]);
+        }
+        else if (current>13) {spelling =ones1[current%10]+"teen ";}
+        else if (current==11) {spelling ="eleven ";}
+        else if (current==12) {spelling ="twelve ";}
+        else if (current==13) {spelling ="thirteen ";}
+        else{
+            spelling =(ones[current]);
+        }
+        if (current==1) {
+        b.append(current).append(" bottle of beer on the wall, ").append(current).append(" bottle of beer.\n");}
+        if ( current!=1) {b.append(current).append(" bottles of beer on the wall, ").append(current).append(" bottles of beer.\n");}
+        b.append("Take " +spelling+"down and pass around, no more bottles of beer on the wall.\n");
+        b.append("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
+
+       return b.toString();
+
+    }
+    public static void main (String[] args){
+        String Lyrics = new BottleSong(40).getBottleSongLyrics();
+        System.out.print(Lyrics);
     }
 }
